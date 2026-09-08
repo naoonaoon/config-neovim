09af5fb354bca9f1f0a44056be54c90bda14d8b0
naoonaoon <naon.dude@icloud.com>
2026-09-08 23:42:05 +0900

    feat: add git support

--- lua/plugin/minis.lua
+++ lua/plugin/minis.lua
@@ -13,6 +13,9 @@ require('mini.bufremove').setup()
 -- Editor
 require('mini.pairs').setup()
 
+-- Git Support
+require('mini.diff').setup()
+
 -- User Interface
 require('mini.icons').setup()
 require('mini.statusline').setup()
