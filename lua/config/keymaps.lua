a37b6f6f15308ff2078299ba5226602667413923
naoonaoon <naon.dude@icloud.com>
2026-09-08 23:32:52 +0900

    feat: rewrite tab shift config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,18 @@ vim.keymap.set('n', '<leader>f', function()
   MiniPick.builtin.files()
 end, { desc = 'File Reference' })
 
+-- Buffer
 vim.keymap.set('n', '<leader>bd', function()
   MiniBufremove.delete()
 end, { desc = 'Close Down Buffer' }) 
+
+vim.keymap.set('n', 'H', '<cmd>bprevious<cr>', {
+  desc = 'Previous Buffer'
+})
+
+vim.keymap.set('n', 'L', '<cmd>bnext<cr>', {
+  desc = 'Next Buffer'
+})
 
 -- Terminal
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>', {
