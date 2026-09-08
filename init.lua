28c329a210b8106b4a0e6df187d43963841fcbcf
naoonaoon <naon.dude@icloud.com>
2026-09-09 00:06:24 +0900

    chore: refactor file name

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('plugin.colorschemes')
-require('plugin.minis')
+require('plugin.colorscheme')
+require('plugin.mini')
 require('plugin.lsp')
-require('plugin.terminals')
+require('plugin.terminal')
 
 require('config.options')
 require('config.keymaps')
