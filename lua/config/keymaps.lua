da67b2e3cfc5c9779f4faaa16b9305c6d344aec9
naoonaoon <naon.dude@icloud.com>
2026-09-13 21:22:16 +0900

    chore: update plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,7 @@
 -- explorer
-vim.keymap.set("n", "<leader>ee", function()
+vim.keymap.set("n", "<leader>e", function()
   MiniFiles.open()
 end, { desc = "Open explorer by mini.files" })
-
-vim.keymap.set("n", "<leader>eo", "<cmd>Oil<cr>", {
-  desc = "Open explorer by oil",
-})
 
 -- file tab
 vim.keymap.set("n", "<leader>bd", function()
@@ -32,6 +28,10 @@ end, { desc = "Serch some files by file name" })
 vim.keymap.set("n", "<leader>sg", function()
   require("telescope.builtin").live_grep()
 end, { desc = "Serch some files by text" })
+
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float()
+end, { desc = "Show diagnostic" })
 
 -- git
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", {
