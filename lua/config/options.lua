d6a8a6790ac8c371393780a13548b6cc7e3f54ec
naoonaoon <naon.dude@icloud.com>
2026-09-13 21:30:28 +0900

    feat: add some options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 5
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -7,6 +13,10 @@ vim.opt.tabstop = 2
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.incsearch = true
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
+
+vim.opt.undofile = true
+vim.opt.swapfile = false
